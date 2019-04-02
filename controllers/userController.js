73ce08e41577b33bf141689456ea0ee516814625
zachha <zachha@gmail.com>
2019-04-02 17:52:55 -0400

    Add userController.js and give login/register renders

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+const promisify = require('es6-promisify');
+
+exports.loginForm = (req, res) => {
+    res.render('login', { title: 'Login' });
+}
+
+exports.registerForm = (req, res) => {
+    res.render('register', { title: 'Register' });
+}
