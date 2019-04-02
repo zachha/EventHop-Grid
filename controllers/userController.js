dca1d5567d4b3177fa84f318f3e8d47a59eef376
zachha <zachha@gmail.com>
2019-04-02 19:06:13 -0400

    Update userController.js with validateRegister function

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,3 +7,25 @@ exports.loginForm = (req, res) => {
 exports.registerForm = (req, res) => {
     res.render('register', { title: 'Register' });
 }
+
+exports.validateRegister = (req, res, next) => {
+    req.sanitizeBody('name');
+    req.checkBody('name', 'You must supply a username!').notEmpty();
+    req.checkBody('email', 'That Email is not valid!').notEmpty();
+    req.sanitizeBody('email').normalizeEmail({
+        remove_dots: false,
+        remove_extension: false,
+        gmail_remove_subaddress: false
+    });
+    req.checkBody('password', 'Password Cannot be Blank!').notEmpty();
+    req.checkBody('password-confirm', 'Confirmed Password cannot be blank!').notEmpty();
+    req.checkBody('password-confirm', 'Whoops! Your Passwords do not Match').equals(req.body.password);
+
+    const errors = req.validationErrors();
+    if (errors) {
+        req.flash('error', errors.map(err => err.msg ));
+        res.render('register', { title: 'Register', body: req.body, flashes: req.flash() });
+        return; // stops the function from running
+    }
+    next() // if no errors then continue!
+};
