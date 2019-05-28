e106b46205bd6074d6aed9146fc71010846e06d6
zachha <zachha@gmail.com>
2019-05-28 01:34:23 -0400

    Update eventController.js with events and event route fns

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,4 +1,4 @@
-
+const fs = require('fs');
 
 exports.homePage = async (req, res) => {
     res.render('index', { title: 'Main'});
@@ -11,3 +11,21 @@ exports.contactPage = (req, res) => {
 exports.createPage = (req, res) => {
     res.render('create', { title: 'Create' });
 };
+
+exports.eventsPage = (req, res) => {
+    res.render('events', { title: 'Events' });
+}
+
+exports.getEventBySlug = async (req, res, next) => {
+    // TESTING EVENTS BEFORE DATABASE IS HOOKED UP DELETE BEFORE DEPLOYMENT
+    const events = JSON.parse(fs.readFileSync('./data/eventdata.json', 'utf-8'));
+    const chosen =  await events.filter(event => event.slug == req.params.slug);
+    const event = chosen[0];
+    console.log(event);
+    if (!event) {
+        return next();
+    }
+    // TESTING
+
+    res.render('event', { event, title: event.name } );
+}
